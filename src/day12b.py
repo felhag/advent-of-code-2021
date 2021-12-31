4f9b363ff222e880eb350a01609547a53dac687f
Felix Hagemans <felixhagemans@gmail.com>
2021-12-31 13:19:34 +0100

    day12b

---
+++ src/day12b.py
@@ -0,0 +1,56 @@
+input = '''xx-end
+EG-xx
+iy-FP
+iy-qc
+AB-end
+yi-KG
+KG-xx
+start-LS
+qe-FP
+qc-AB
+yi-start
+AB-iy
+FP-start
+iy-LS
+yi-LS
+xx-AB
+end-KG
+iy-KG
+qc-KG
+FP-xx
+LS-qc
+FP-yi'''
+
+nodes = [line.split('-') for line in input.splitlines()]
+completed = []
+
+
+def other(current, node):
+    return node[1 if node.index(current) == 0 else 0]
+
+
+def find_next(current, visited):
+    connected = filter(lambda node: current in node, nodes)
+    for connect in connected:
+        next_cave = other(current, connect)
+        next_path = visited[:] + [next_cave]
+
+        if next_cave == 'start':
+            continue
+        elif next_cave == 'end':
+            completed.append(next_path)
+        elif next_cave.isupper():
+            find_next(next_cave, next_path)
+        else:
+            small = list(filter(lambda node: node.islower(), next_path))
+            if len(small) - len(set(small)) <= 1:
+                find_next(next_cave, next_path)
+
+
+find_next('start', ['start'])
+
+
+print(f'Completed {len(completed)} paths:')
+# completed.sort()
+# for c in completed:
+#     print(','.join(c))
